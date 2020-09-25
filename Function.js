3bc609d0d243d72b7efde2221eaa8a7a6715f7d4
Angga Chelsea <muhidabdul168@gmail.com>
2020-09-25 20:18:53 +0700

    function.js

--- Function.js
+++ Function.js
@@ -1,31 +1,61 @@
-// function testFunction(awal, akhir) {
-//     let isiNumber = [];
-//     if (awal <= akhir) {
-//         for (let i = awal; i <= akhir; i++) {
-//             isiNumber.push(i);
-//         }
-//     } else {
-//         for (let i = awal; i >= akhir; i--) {
-//             isiNumber.push(i)
-//         }
-//     }
-//     return isiNumber
-// }
-// console.log(testFunction(10, 1))
-// console.log(testFunction(5, 8))
+// // function testFunction(awal, akhir) {
+// //     let isiNumber = [];
+// //     if (awal <= akhir) {
+// //         for (let i = awal; i <= akhir; i++) {
+// //             isiNumber.push(i);
+// //         }
+// //     } else {
+// //         for (let i = awal; i >= akhir; i--) {
+// //             isiNumber.push(i)
+// //         }
+// //     }
+// //     return isiNumber
+// // }
+// // console.log(testFunction(10, 1))
+// // console.log(testFunction(5, 8))
 
-function testFunction(awal, akhir) {
-    let isiNumber = [];
-    if (awal <= akhir) {
-        for (let i = awal; i <= akhir; i++) {
-            isiNumber.push(i);
-        }
-    } else {
-        for (let i = awal; i >= akhir; i--) {
-            isiNumber.push(i)
-        }
-    }
-    return isiNumber
-}
-console.log(testFunction(1, 6))
-console.log(testFunction(180, 191))
+// // function testFunction(awal, akhir) {
+// //     let isiNumber = [];
+// //     if (awal <= akhir) {
+// //         for (let i = awal; i <= akhir; i++) {
+// //             isiNumber.push(i);
+// //         }
+// //     } else {
+// //         for (let i = awal; i >= akhir; i--) {
+// //             isiNumber.push(i)
+// //         }
+// //     }
+
+// //     return isiNumber
+// // }
+// // console.log(testFunction(1, 6))
+// // console.log(testFunction(180, 191))
+
+// const harga = [{
+//         name: 'waper',
+//         harga: 200
+//     },
+//     {
+//         name: 'kukun',
+//         harga: 300
+//     },
+// ];
+
+// callBack
+// const beli = (saldo, name, cb) => {
+//     const i = harga.findIndex((e) => e.name === name);
+//     if (i > 0) {
+//         const sisaSaldo = (saldo - harga[i].harga);
+//         cb(null, sisaSaldo);
+//     } else {
+//         cb('jajan entong')
+//     }
+// };
+
+// beli(1000, ' waper', (e, sisaSaldo) => {
+//     if (e) {
+//         console.log('err', e);
+//     } else {
+//         console.log('berhasil', sisaSaldo);
+//     }
+// })
